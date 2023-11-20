7833eb6c0f44f364ba7cb06d71d1337a4cfe55c3
Jaimi Patel <jaimipatel77@gmail.com>
2023-11-20 14:36:49 +0530

    Component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+// import component
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
 </script>
 
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
+    <!-- use component  -->
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
     </div>
