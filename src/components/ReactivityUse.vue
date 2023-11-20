dc47b4d8afdfedaec1069a038d0d7ac0eafdb9e9
Jaimi Patel <jaimipatel77@gmail.com>
2023-11-20 16:05:20 +0530

    Reactivity : ref

--- src/components/ReactivityUse.vue
+++ src/components/ReactivityUse.vue
@@ -1,17 +1,29 @@
 <template>
   <div>
     <!-- Reactivity : reactive -->
-    {{ obj }}
+    obj : {{ obj }}
     <p>Sum : {{ obj.a + obj.b }}</p>
     <!-- Reactivity : reactive : deep reactivity  -->
     <p>
-      <b>{{ arr }}</b>
+      <b>arr : {{ arr }}</b>
     </p>
+
+    <!-- Reactivity : ref -->
+    <p>count : {{ count }}</p>
+    <p>object.count : {{ object1.count }}</p>
+    <p>object.count : {{ object2.count }}</p>
+    <p>object.count : {{ object2.count }}</p>
+
+    <!-- Reactivity : ref : distructure -->
+    <p>distructure : {{ distructure }}</p>
+
+    <!-- use as expretion  -->
+    <p>object.count : {{ object2.count.value + 10 }}</p>
   </div>
 </template>
 
 <script setup>
-import { reactive } from "vue";
+import { reactive, ref } from "vue";
 
 const obj = reactive({ a: 10, b: 20 });
 setTimeout(() => {
@@ -23,4 +35,25 @@ setTimeout(() => {
   arr[0].count = 30;
   arr[1].push(10);
 }, 2000);
+
+const count = ref(0);
+setTimeout(() => {
+  count.value = 30;
+}, 2000);
+
+const object1 = ref({ count: 0 });
+setTimeout(() => {
+  object1.value.count = 30;
+}, 2000);
+
+const object2 = { count: ref(0) };
+setTimeout(() => {
+  object2.count.value = 30;
+}, 2000);
+
+const object = { distructure: ref(0) };
+setTimeout(() => {
+    object.count.value = 30;
+}, 2000);
+let { distructure } = object;
 </script>
