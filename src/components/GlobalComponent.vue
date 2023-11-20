c642b318d9d425643c43719a84019d4bbf322e4d
Jaimi Patel <jaimipatel77@gmail.com>
2023-11-20 15:15:01 +0530

    Attribute Binding

--- src/components/GlobalComponent.vue
+++ src/components/GlobalComponent.vue
@@ -3,9 +3,37 @@
   <div>
     {{msg}}
     <p>{{ 10+19 }}</p>
+    <!-- Attribute Binding  -->
+    <a :href="googleUrl">Use Google</a>
+    <br />
+    <a v-bind:href="googleUrl" target="_blank">Use Google - target</a>
+    <!-- Use Multiple Attribute  -->
+    <p v-bind="style">Hello style</p>
+    <!-- Use Dynamic Attribute  -->
+    <p v-bind:[styleName]="'colour-pink'">Pink</p>
+    <p :[styleName]="'colour-yellow'">Pink</p>
+    <p></p>
   </div>
 </template>
 
 <script setup>
 const msg = "Hello, there !";
+const googleUrl = "https://www.google.co.in/";
+const style = {
+  id: "style",
+  class: "colour",
+};
+const styleName = "class";
 </script>
+
+<style scoped>
+.colour{
+  color: red;
+}
+.colour-pink{
+  color: palevioletred;
+}
+.colour-yellow{
+  color: yellowgreen;
+}
+</style>
